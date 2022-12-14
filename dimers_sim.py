2f3abce4353ad259c2c3fd49ff44b41deb0284f7
oarzi <ofir.arzi@gmail.com>
2022-12-14 15:07:07 +0100

    once again and again

--- dimers_sim.py
+++ dimers_sim.py
@@ -6,90 +6,7 @@ import matplotlib.pyplot as plt
 import time
 from tqdm import tqdm
 import os
-
-def quantum_evolution(L, times, H, d=0, J=1):
-    H_ring, H_hopp, configs = H['H_ring'], H['H_hopp'], H['configs']
-    dim = H_ring.shape[0]
-    psi, i0 = get_initial_state(L, configs, dim, d)
-    
-
-    dt = 0.5
-    rho = [defect_density(configs,psi)]
-
-    for i in  tqdm(range(times)):
-        psi = expm_multiply(-1j*dt*H_ring,psi)
-        psi = expm_multiply(-1j*dt*J*H_hopp,psi)
-
-        rho.append(defect_density(configs,psi))
-
-    rho = np.array(rho)
-    
-    return rho
-
-def classical_evolutions_single(L, times, H, d, _p):
-    H_ring, H_hopp, configs = H['H_ring'], H['H_hopp'], H['configs']
-    
-    psi, i = get_initial_state(L, configs, H_ring.shape[0], d)
-    rho = np.array([defect_density(configs,psi)])
-    
-    
-    steps = [i]
-    for _ in  tqdm(range(times)):
-        rings_i = list(H_ring.getrow(i).nonzero()[1])
-        hopps_i = list(H_hopp.getrow(i).nonzero()[1])
-        p_array = np.concatenate((np.ones(len(rings_i)), _p*np.ones(len(hopps_i))))/(len(rings_i)+_p*len(hopps_i))
-        i = np.random.choice(rings_i + hopps_i, p=p_array)
-        curr_psi = np.zeros(psi.shape)
-        curr_psi[i] = 1.
-
-        rho = np.row_stack((rho ,defect_density(configs,curr_psi)))
-        steps.append(i)
-    
-    return (rho, steps)
-
-def classical_evolutions_single2(L, times, H, d, _p):
-    H_ring, H_hopp, = H['H_ring'], H['H_hopp']
-    gates = H_ring + H_hopp
-    p_array = np.concatenate((np.ones(len(H_ring)), _p*np.ones(len(H_hopp))))/(len(H_ring)+_p*len(H_hopp))
-    psi =  get_initial_config(L, d)
-    rho = np.array([defect_density(psi)])
-    
-    gate = np.random.choice(gates, size=times, p=p_array)
-    
-    for gate in tqdm(gates, miniters=20):
-        # gate = np.random.choice(gates, p=p_array)
-        psi = gate(psi)
-        rho = np.row_stack((rho ,defect_density(psi)))
-    return rho
-
-def classical_evolution(L, times, H, d=0, nums=1, steps=False, p=1):
-    print("id: {}, L =  {}, times = {}, d = {}, nums = {}".format(os.getpid(), L, times, d, nums))
-    start = time.process_time()
-    vc = np.vectorize(classical_evolutions_single2, otypes='O', cache=True)
-    
-    results = vc([L]*nums, [times]*nums, [H]*nums, [d]*nums, [p]*nums)
-    
-    rhos = np.array(results)
-    rhos = np.sum(rhos, axis=0)/nums
-    
-    end = time.process_time()
-    print("Elapsed time during the {} in seconds: {}".format(os.getpid(), end-start)) 
-    return (rhos, [res for res in results[1]]) if steps else rhos
-
-
-
-def parallel_analysis(L, times, d, nums):
-    H_ring, H_hopp = get_h_ring(L), get_h_hop(L)
-    print(len(H_ring), len(H_hopp))
-    H = {'H_ring' : H_ring, 'H_hopp' : H_hopp}
-    with Pool(6) as p:
-        c_rhos =  p.starmap(classical_evolutions_single3, ((L, times, H, d, nums) for d in d), chunksize=1)
-        p.close()
-        p.join()
-    analysis_rhos =  [analyze(rho) for rho in c_rhos]
-    with open('analysis_L{}_t{}_d{}.pickle'.format(L, times, time.strftime("%Y_%m_%d_%H_%M")), 'wb') as handle:
-        pickle.dump(analysis_rhos, handle)
-    return analysis_rhos
+from itertools import repeat
 
 @dataclass
 class Simulator:
@@ -100,15 +17,20 @@ class Simulator:
     prob : int = 1
     local: bool = True
     d_procs_num : int = 1
-    nums_subprocs_num : int = 1
+    batch_subprocs_num : int = 1
+    save : bool = True
+    analysis_rhos : list[dict] = None
     
-    def __post_init__(self):
-        self.analysis_rhos = []
 
     def progress_bar(self, iterable):
-        return tqdm(iterable, miniters= self.times//25) if self.local else iterable
+        if self.local:
+            tqdm_text = "#" + ("{}->".format(os.getppid()) + "{}".format(os.getpid())).ljust(12) + " "
+            return tqdm(iterable, miniters= self.times//25, desc=tqdm_text) 
+        else:
+            return iterable
     
     def parallel_analysis(self):
+        self.analysis_rhos = []
         H = {'H_ring' : get_h_ring(self.L), 'H_hopp' : get_h_hop(self.L)}
         print(len(H['H_ring']), len(H['H_hopp']))
         manager = Manager()
@@ -136,16 +58,18 @@ class Simulator:
         
         while not queue.empty():
             self.analysis_rhos.append(queue.get())
-
-        with open('analyses/analysis_L{}_t{}_d{}.pickle'.format(self.L, self.times, time.strftime("%Y_%m_%d_%H_%M")), 'wb') as handle:
-            pickle.dump(self.analysis_rhos, handle)
+        
+        if self.save:
+            with open('analyses/analysis_L{}_t{}_d{}.pickle'.format(self.L, self.times, time.strftime("%Y_%m_%d__%H_%M")), 'wb') as handle:
+                pickle.dump(self.analysis_rhos, handle)
+        print("Finished parallel_analysis for  L =  {}, times = {}, d = {}, nums = {}".format(self.L, self.times, self.d, self.nums))
         return self.analysis_rhos
     
     def classical_evolution(self, H, _d, q, sema):
         print("id: {}, L =  {}, times = {}, d = {}, nums = {}".format(os.getpid(), self.L, self.times, _d, self.nums))
         start = time.process_time()
-        with Pool(self.nums_subprocs_num) as p:
-            c_rhos =  p.starmap(self.classical_evolutions_nums, ((H, _d) for i in range(self.nums_subprocs_num)), chunksize=1)
+        with Pool(self.batch_subprocs_num) as p:
+            c_rhos =  p.starmap(self.classical_evolutions_batch, repeat((H, _d), self.batch_subprocs_num), chunksize=1)
             p.close()
             p.join()
         
@@ -162,28 +86,40 @@ class Simulator:
         return 0
 
     
-    def classical_evolutions_nums(self, H, _d):
+    def classical_evolutions_batch(self, H, _d):
         H_ring, H_hopp, = H['H_ring'], H['H_hopp']
 
         p_array = np.concatenate((np.ones(len(H_ring)),self.prob*np.ones(len(H_hopp))))/(len(H_ring)+self.prob*len(H_hopp))
         allgates = H_ring + H_hopp
 
-        initial_psi = get_initial_config(self.L, _d)
-        psi = np.array([[initial_psi]*(self.nums//self.nums_subprocs_num)], dtype=np.int32)
+        initial_psi = [get_initial_config(self.L, _d)]*(self.nums//self.batch_subprocs_num)
+        psi = np.array(initial_psi, dtype=np.int32)
+        # print("psi0.shape=", psi.shape)
+        
+        rho = np.apply_along_axis(defect_density, 1 , psi)
+        rho = np.sum(rho, axis=0).reshape(1, self.L)
+        
+        # print("rho0.shape=", rho.shape)
 
         def apply(f):
             return f[0](f[1])
 
         for _ in self.progress_bar(range(self.times)):
+            # print("===================")
             gates_i = np.random.choice(allgates, size=self.nums, p=p_array)
-            psi_next = np.array(list(map(apply, zip(gates_i, psi[-1]))))
-            psi = np.vstack((psi, [psi_next]))
+            psi = np.array(list(map(apply, zip(gates_i, psi))))
+            # print("psi.shape=", psi.shape)
+            charge = np.apply_along_axis(defect_density, 1 , psi)
+            # print("charge.shape=", charge.shape)
+            rho = np.vstack((rho, np.sum(np.apply_along_axis(defect_density, 1 , psi), axis=0).reshape(1, self.L)))
+
+            # psi = np.vstack((psi, [psi_next]))
+            # print("rho.shape=", rho.shape)
+
         
-        print("{}->{} before= {}".format(os.getppid(), os.getpid(), psi.shape))
-        rho = np.apply_along_axis(defect_density, 2 , psi)
-        print("charge=", rho.shape)
-        rho = np.sum(rho, axis=1)
-        print("after", rho.shape)
+        # print("{}->{} before= {}".format(os.getppid(), os.getpid(), psi.shape))
+        # print("{} batch rho={}".format(os.getppid(), rho.shape))
+
         return rho
 
     def analyze(self, rho):
