10fe0a2806968c85fcca86b1c006203bfb560fc3
Arzi <ge83qez@ph.tum.de>
2022-12-14 15:57:38 +0100

    running on cluster

--- parallel_analysis.py
+++ parallel_analysis.py
@@ -14,15 +14,15 @@ from dimers_util import *
 from multiprocessing import Pool
 
 def main():
-    d_sim = [55, 65 ,75, 85, 95]
+    d_sim = [55, 65, 75, 85, 95]
     L_sim = 100
-    times_sim = 5000
-    nums_sim = 10000
+    times_sim = 1000
+    nums_sim = 120000
     d_procs_sim = 5
     nums_subprocs_sim = 10
-    simulator = dimers_sim.Simulator(local = False,L=L_sim, times=times_sim, d=d_sim, nums=nums_sim, d_procs_num=d_procs_sim, nums_subprocs_num = nums_subprocs_sim)
+    simulator = dimers_sim.Simulator(local = False,L=L_sim, times=times_sim, d=d_sim, nums=nums_sim, d_procs_num=d_procs_sim, batch_subprocs_num = nums_subprocs_sim)
     simulator.parallel_analysis()
-    # dimers_sim.plot_analysis(simulator.analysis_rhos, d_sim, L_sim, times_sim, nums_sim)
+    dimers_sim.plot_analysis(simulator.analysis_rhos, L_sim, times_sim, nums_sim, save=True)
 if __name__ == '__main__':
 	main()
 
