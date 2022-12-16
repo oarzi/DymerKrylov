4967dd11b2182cfbb4fe3693e7c1bec3d5e26439
Arzi <ge83qez@ph.tum.de>
2022-12-16 10:25:29 +0100

    after long run

--- parallel_analysis.py
+++ parallel_analysis.py
@@ -3,8 +3,8 @@ import dimers_sim
 def main():
     d_sim = [70, 95, 120, 145]
     L_sim = 150
-    times_sim = 3000
-    nums_sim = 999990
+    times_sim = 6000
+    nums_sim = 2*999990
     d_procs_sim = 4
     nums_subprocs_sim = 30
     
