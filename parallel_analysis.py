fdbb2ce295104bdbf732d11f477c457f08cd8907
Arzi <ge83qez@ph.tum.de>
2022-12-19 11:07:06 +0100

    cluster finished running. many new results and output files

--- parallel_analysis.py
+++ parallel_analysis.py
@@ -1,10 +1,10 @@
 import dimers_sim
 
 def main():
-    d_sim = [70, 95, 120, 145]
-    L_sim = 150
+    d_sim = [115, 95, 75, 50]
+    L_sim = 120
     times_sim = 6000
-    nums_sim = 2*999990
+    nums_sim = 1500000
     d_procs_sim = 4
     nums_subprocs_sim = 30
     
